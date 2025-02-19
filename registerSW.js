36226b9cef310c1b61f9c1c9fc433ee07e83591a
Bram van der Meij <bram@newstory.nl>
2025-02-19 09:16:47 +0100

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/FASK-PWA/study-with-miffy/sw.js', { scope: '/FASK-PWA/study-with-miffy/' })})}
